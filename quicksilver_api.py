3a3a69e09f8026e93781718fb9695bc03613a3e7
Chris J. Karr <chris@audacious-software.com>
2021-10-12 14:09:42 +0000

    Specified task queue for Quicksilver task setup

--- quicksilver_api.py
+++ quicksilver_api.py
@@ -1,4 +1,4 @@
 def quicksilver_tasks():
     return [
-        ('simple_data_export_compile_reports', '--no-color', 60,),
+        ('simple_data_export_compile_reports', '--no-color', 60, 'data-export'),
     ]
