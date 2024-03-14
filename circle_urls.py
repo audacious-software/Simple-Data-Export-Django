c9563b7e130fa50074273e2a884e4c2d21ae4612
Chris J. Karr <chris@audacious-software.com>
2024-03-14 10:57:55 -0500

    Circle CI fix

--- circle_urls.py
+++ circle_urls.py
@@ -1,6 +1,13 @@
-import django
+# pylint: disable=no-name-in-module, wrong-import-position
 
-from django.conf.urls import include, url
+import sys
+
+if sys.version_info[0] > 2:
+    from django.urls import re_path as url, include # pylint: disable=no-name-in-module
+else:
+    from django.conf.urls import url, include
+
+import django
 
 urlpatterns = [
     url(r'^admin/', django.contrib.admin.site.urls),
